4a0d29b26048d691f4a24d90df9b9c6f2ac317c2
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-04-06 11:03:51 -0300

    Adding exercise 2

---
+++ Fase 1/Exec_2.rs
@@ -0,0 +1,8 @@
+// Declare uma variável mutável e altere seu valor.
+fn main() {
+    let mut var: u64 = 5;
+    println!("Valor inicial: {}", var);
+    
+    var = 10;
+    println!("Valor alterado: {}", var);
+}
