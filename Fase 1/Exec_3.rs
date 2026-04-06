c8d0df4ce182dd3a8c85e1c1abb23ba56ac19884
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-04-06 11:07:45 -0300

    adding exercise 3

---
+++ Fase 1/Exec_3.rs
@@ -0,0 +1,8 @@
+// Crie um programa que imprima o valor máximo de um u32 e um i32.
+fn main(){
+    let max_u32 = u32::MAX;
+    let max_i32 = i32::MAX;
+    
+    println!("Valor máximo de u32: {}", max_u32);
+    println!("Valor máximo de i32: {}", max_i32);
+}
