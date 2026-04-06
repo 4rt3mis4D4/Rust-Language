1af59a66bec11be4c2865549693fcc322afc1c70
Artemis <130113421+4rt3mis4D4@users.noreply.github.com>
2026-04-06 17:13:29 -0300

    Exercise correction

--- Fase 1/Exec_7.rs
+++ Fase 1/Exec_7.rs
@@ -2,4 +2,8 @@
 fn main(){
     let tupla = (500, 6.4, "Hello");
     println!("Tupla: {:?}", tupla);
+
+    println!("Primeiro valor: {}", tupla.0);
+    println!("Segundo valor: {}", tupla.1);
+    println!("Terceiro valor: {}", tupla.2);
 }
